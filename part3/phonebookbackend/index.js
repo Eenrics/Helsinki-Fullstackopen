45e082a058a383de6e1525a9e64467a56d3e4fe5
Ebenezer Eshetie <77492217+EbenGitHub@users.noreply.github.com>
2023-02-16 13:54:31 +0300

    mongodb

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,8 +1,8 @@
-// const { response } = require('express')
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-
+const Phone = require('./models/phone')
 
 const app = express()
 
@@ -15,93 +15,72 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 const idGen = () => Math.floor((Math.random() * 12) * 30)
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Phone.find({}).then(phone => {
+        res.json(phone)
+    })
 })
 
 app.get('/info', (req, res) => {
-    let response = `<p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date}</p>`
-    
-    res.send(response)
+    Phone.find({})
+        .then(phones => {
+            let response = `<p>Phonebook has info for ${phones.length} people</p>
+            <p>${new Date}</p>`
+            res.send(response)
+        })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    let id = parseInt(req.params.id)
-    let person = persons.find(per => per.id === id)
-    
-    if (person) return res.json(person)
-    else return res.status(404).json({error: "person not found"}).end()
+app.get('/api/persons/:id', (req, res, next) => {
+    Phone.findById(req.params.id)
+    .then(phone => {
+        if (phone) {
+            return res.json(phone)
+        } else {
+            return res.status(404).json({error: "person not found"}).end()
+        }
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res) => {
     let person = req.body
-    let perExis = persons.find(per => per.name === person.name)
+    let perExis = Phone.find({name: person.name}).then(data => console.log(data, typeof data))
 
     if (!person.name || !person.number) return res.status(400).json({error: "name or number is missing"}).end()
-    else if (perExis) return res.status(409).json({error: "name must be unique"}).end()
+    else if (perExis.length) return res.status(409).json({error: "name must be unique"}).end()
 
-    let newPerson = {
-        id: idGen(),
-        name: person.name,
-        number: person.number
-    }
-    persons = persons.concat(newPerson)
-    
-    res.status(201).json(newPerson)
+    let phone = new Phone({name: person.name, number: person.number})
+    phone.save()
+      .then(response => {
+        res.json(response)
+      })
+      .catch(error => {
+        console.log("error saving data to mongodb", error.message)
+      })
 })
 
-app.put('/api/persons/:id', (req, res) => {
-    let id = parseInt(req.params.id)
-    let person = req.body
-    let perExis = persons.find(per => per.id === id)
-    let perConf = persons.find(per => (per.name === person.name && per.id !== id))
-    
-    if (!perExis) return res.status(400).json({error: "no person found with this id"}).end()
-    else if (perConf) return res.status(400).json({error: "name in use by other id"}).end()
+app.put('/api/persons/:id', (req, res, next) => {
+    person = req.body
 
     let newPer = {
-        id,
         name: person.name,
         number: person.number
     }
-    persons = persons.map(per => {
-        if (per.id !== id) return per
-        else return newPer
-    })
-    
-    res.status(200).json(newPer)
+
+    Phone.findByIdAndUpdate(req.params.id, newPer, { new: true})
+        .then(updatedPhon => {
+          res.status(200).json(updatedPhon)
+        })
+        .catch(err => next(err))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-    let id = parseInt(req.params.id)
-    persons = persons.filter(per => per.id !== id)
-    
-    res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+    Phone.findByIdAndRemove(req.params.id)
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(err => next(err))
 })
 
 const unknownEndpoint = (req, res) => {
@@ -109,6 +88,18 @@ const unknownEndpoint = (req, res) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (err, req, res, next) => {
+    console.log(err.message)
+
+    if (err.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(err)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
