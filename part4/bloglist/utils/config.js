bc866b7b396e982810b64e5659f474ddbc0cd095
Ebenezer Eshetie <77492217+EbenGitHub@users.noreply.github.com>
2023-02-18 02:02:35 +0300

    token test add

--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
-const url = process.env.MONGODB_URI
+const url = process.env.NODE_ENV === 'production' ?
+                 process.env.MONGODB_URI : process.env.TEST_MONGODB_URI
 const PORT = process.env.PORT
 
 module.exports = {url, PORT}
